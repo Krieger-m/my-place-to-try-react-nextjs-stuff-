6d3597c66f367abb4cb63f8116ea2ee50a3b790d
Mat <matze.krieger@googlemail.com>
2025-11-20 12:01:11 +0100

    added home picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,30 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { cacheLife } from "next/cache";
+import projectHeroImg from "../../public/projectHeroImg.png";
+
 
 export default async function Home() {
+
   
   cacheLife("days");
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h3>Home</h3>
+        <Image
+          src={projectHeroImg}
+          alt="no alt text"
+          height={1036}
+          width={1024}
+          style={{
+            width: '200vw',
+            height: '80vh',
+            position: 'relative',
+            right:0,
+            left: '-100%',
+          }}
+        />
       </main>
     </div>
   );
